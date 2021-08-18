464180dfb8fbada14853708cb516d0d092d112d9
purplehays09 <purplehays527@gmail.com>
2021-08-18 10:26:43 -0600

    Failure of Post parameters
    I didn't really have issues until I got to the post create. This is erroring due to 'param is missing or the value is empty : user'. This is a little confusing since I am not sure what it needs to pass in here. From what I can tell it only needs the name, email, and phone_number. I am not sure if I should just pass in a User.new() like what I see online bu I am not sure if that is what I need to do here.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,17 +18,51 @@ RSpec.describe UsersController do
   end
 
   describe "GET show" do
-    it "returns 200" do
-      User.create(
+    it "returns a users name" do
+      user = User.create(
         name:"Alphonse",
         email: "Corli@one.com",
         phone_number: "0987656789"
       )
-
-      get 'users/1'
-
-      expect(response.name).to eq("Alphonse")
+      
+      get :show, params: { id: user.id }
+      
+      expect(response).to be_successful
     end
-    
   end
+  
+  describe "GET new" do
+    it "returns a new blank user" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+  
+  describe "GET edit" do
+    it "returns 200" do
+      user = User.create(
+        name:"Alphonse",
+        email: "Corli@one.com",
+        phone_number: "0987656789"
+      )
+      get :edit, params: {id:user.id}
+      
+      expect(response).to be_successful
+    end
+  end
+  
+  describe "POST create" do
+    it "posts a user and returns a sucess" do
+      user = {
+        name:"Alphonse",
+        email: "Corli@one.com",
+        phone_number: "0987656789"
+      }
+      post :create, params: user
+
+      expect(response).to be_successful
+    end
+  end
+  
+  
 end
